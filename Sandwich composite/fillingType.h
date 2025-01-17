d0cb9be218b842adf6d70a6265aa168ef1277848
Spectre <fardeenjr49@gmail.com>
2025-01-17 00:47:12 -0500

    just comments

--- Sandwich composite/fillingType.h
+++ Sandwich composite/fillingType.h
@@ -7,6 +7,8 @@ using namespace std;
 class fillingType
 {
 public:
+
+	//constructor
 	fillingType();
 	fillingType(bool peanutbutter, bool jelly);
 	fillingType(int cheeseNum, int cheeseType, bool roast);
@@ -21,19 +23,28 @@ public:
 	//Postcondition: Calls functions from meat
 
 	string returnMeat() const;
+	//lists all the meat in the sandwich
+	//post: returns string containing all variable names in meat object set to true
 
 	void veggies(bool choice[6]);
 	//determines which veggies types will be used.
 	//Postcondition: Calls functions from veggies
 
 	string returnVeggies() const;
+	//lists all the veggies in the sandwich
+	//post: returns string containing all variable names in veggies object set to true
 
 	void roast(bool roast);
 	//determines if to use roasted ingredients
 	//Postcondition: sets roasted to true or false
 
 	bool returnRoastBool() const;
+	//sends back boolian if roasted or not
+	//post: returns bool roasted
+	
 	string returnRoastString() const;
+	//sends a string confirming if roasted is true or false
+	//post: returns string "roasted" or "not roasted" on condition of bool roasted
 
 private:
 	
